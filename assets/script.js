75d15c354caf85d872bb7e89c4335d6860e9d789
Grant-Ellington <gellingtonem6@gmail.com>
2022-09-21 11:50:33 -0500

    game works with prompts and console, need to add alerts and score card on the front

--- assets/script.js
+++ assets/script.js
@@ -3,13 +3,49 @@ var start = document.getElementById('start');
 
 start.addEventListener("click", startFunction);
 function startFunction() {
-  console.log('it works')
   computerPicks();
 }
 
 function computerPicks () {
-    var computerPlay = Math.floor(Math.random()*3);
-    console.log(computerPlay)
+    var computerNumber = Math.floor(Math.random()*3);
+    console.log(computerNumber)
+    play(computerNumber);
+
 }
 
- 
+var computerPlay = "";
+
+function play (computerNumber){
+    if(computerNumber ===0){
+         computerPlay = 'r'
+    }else if(computerNumber === 1){
+        computerPlay = 'p'
+    }else{
+         computerPlay = 's'
+    }
+    console.log(computerPlay)
+    var personPlay = prompt("please select wither rock by typing r, paper by entering p, or scissors by entering s", "");
+    gameTime(computerPlay, personPlay)
+}
+
+function gameTime (computerPlay, personPlay) {
+    if(personPlay === computerPlay){
+        console.log('its a tie')
+    }else if(personPlay === 'r' && computerPlay === "s" ){
+        console.log( "you win")
+    }else if(personPlay === 'p' && computerPlay === "r" ){
+        console.log('you win')
+    }else if(personPlay === 's' && computerPlay === "p" ){
+        console.log('you win')
+    }else if(personPlay === 's' && computerPlay === "r" ){
+        console.log('you lose')
+    }else if(personPlay === 'r' && computerPlay === "p" ){
+        console.log('you lose')
+    }else{
+        console.log('you lose')
+    }
+
+}
+
+
+
